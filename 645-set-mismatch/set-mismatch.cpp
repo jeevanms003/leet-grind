64cb7a316d82034080e649d1dfaa7c6f3edd3922
Jeevan M Swamy <jeevanms003@gmail.com>
2026-01-03 11:22:48 +0530

    Time: 0 ms (100.00%) | Memory: 26.5 MB (33.86%) - LeetSync

--- 645-set-mismatch/set-mismatch.cpp
+++ 645-set-mismatch/set-mismatch.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     vector<int> findErrorNums(vector<int>& nums) {
         int n = nums.size();
-        vector<int> count(n + 1, 0);
+        vector<int> count(n + 2, 0);
 
         for (int x : nums)
             count[x]++;
