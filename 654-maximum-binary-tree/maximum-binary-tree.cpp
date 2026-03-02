93b775b09d01431cd6585c8a53d0dbd07396cd57
Jeevan M Swamy <jeevanms003@gmail.com>
2026-03-02 15:28:17 +0530

    Time: 13 ms (43.20%) | Memory: 44 MB (31.69%) - LeetSync

--- 654-maximum-binary-tree/maximum-binary-tree.cpp
+++ 654-maximum-binary-tree/maximum-binary-tree.cpp
@@ -10,57 +10,27 @@
 class Solution {
 public:
 
-    int findMaxIndex(vector<int>& nums, int l, int r){
-        int index = l;
+    TreeNode* dfs(vector<int>& nums, int l, int r){
+
+        if(l > r)
+            return NULL;
+
+        // Find max element index
+        int maxIndex = l;
         for(int i=l;i<=r;i++){
-            if(nums[i] > nums[index])
-                index = i;
+            if(nums[i] > nums[maxIndex])
+                maxIndex = i;
         }
-        return index;
+
+        TreeNode* root = new TreeNode(nums[maxIndex]);
+
+        root->left = dfs(nums, l, maxIndex-1);
+        root->right = dfs(nums, maxIndex+1, r);
+
+        return root;
     }
 
     TreeNode* constructMaximumBinaryTree(vector<int>& nums) {
-
-        if(nums.empty())
-            return NULL;
-
-        // Root = maximum of entire array
-        int rootIndex = findMaxIndex(nums,0,nums.size()-1);
-        TreeNode* root = new TreeNode(nums[rootIndex]);
-
-        // Queue stores node and subarray range
-        queue<pair<TreeNode*, pair<int,int>>> q;
-
-        q.push({root,{0,nums.size()-1}});
-
-        while(!q.empty()){
-
-            auto curr = q.front();
-            q.pop();
-
-            TreeNode* node = curr.first;
-            int left = curr.second.first;
-            int right = curr.second.second;
-
-            int maxIndex = findMaxIndex(nums,left,right);
-
-            // Left subtree
-            if(left <= maxIndex-1){
-                int leftMax = findMaxIndex(nums,left,maxIndex-1);
-                node->left = new TreeNode(nums[leftMax]);
-
-                q.push({node->left,{left,maxIndex-1}});
-            }
-
-            // Right subtree
-            if(maxIndex+1 <= right){
-                int rightMax = findMaxIndex(nums,maxIndex+1,right);
-                node->right = new TreeNode(nums[rightMax]);
-
-                q.push({node->right,{maxIndex+1,right}});
-            }
-        }
-
-        return root;
+        return dfs(nums,0,nums.size()-1);
     }
 };
