cf04249abdb3ffedc83292113dfde5c7a2867c7a
Jeevan M Swamy <jeevanms003@gmail.com>
2026-03-09 03:33:37 +0530

    Time: 7 ms (52.46%) | Memory: 15.3 MB (27.64%) - LeetSync

--- 138-copy-list-with-random-pointer/copy-list-with-random-pointer.cpp
+++ 138-copy-list-with-random-pointer/copy-list-with-random-pointer.cpp
@@ -1,60 +1,29 @@
-/*
-// Definition for a Node.
-class Node {
-public:
-    int val;
-    Node* next;
-    Node* random;
-    
-    Node(int _val) {
-        val = _val;
-        next = NULL;
-        random = NULL;
-    }
-};
-*/
-
 class Solution {
 public:
     Node* copyRandomList(Node* head) {
-        
+
         if(head == NULL)
             return NULL;
 
+        unordered_map<Node*, Node*> mp;
+
         Node* curr = head;
 
-        // 1. Insert copied nodes
+        // 1. Create copy nodes
         while(curr) {
-            Node* copy = new Node(curr->val);
-            copy->next = curr->next;
-            curr->next = copy;
-            curr = copy->next;
-        }
-
-        // 2. Assign random pointers
-        curr = head;
-        while(curr) {
-            if(curr->random)
-                curr->next->random = curr->random->next;
-
-            curr = curr->next->next;
-        }
-
-        // 3. Separate the lists
-        curr = head;
-        Node* dummy = new Node(0);
-        Node* copyCurr = dummy;
-
-        while(curr) {
-            Node* copy = curr->next;
-
-            curr->next = copy->next;
-            copyCurr->next = copy;
-
-            copyCurr = copy;
+            mp[curr] = new Node(curr->val);
             curr = curr->next;
         }
 
-        return dummy->next;
+        curr = head;
+
+        // 2. Assign next and random
+        while(curr != NULL) {
+            mp[curr]->next = mp[curr->next];
+            mp[curr]->random = mp[curr->random];
+            curr = curr->next;
+        }
+
+        return mp[head];
     }
 };
