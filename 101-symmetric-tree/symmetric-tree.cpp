e16b9ea146189412125f19896b227ee32bcd0a9b
Jeevan M Swamy <jeevanms003@gmail.com>
2026-03-09 13:04:13 +0530

    Time: 0 ms (100.00%) | Memory: 18.7 MB (9.24%) - LeetSync

--- 101-symmetric-tree/symmetric-tree.cpp
+++ 101-symmetric-tree/symmetric-tree.cpp
@@ -5,37 +5,30 @@ public:
         if(root == NULL)
             return true;
         
-        queue<TreeNode*> q1, q2;
+        queue<TreeNode*> q;
         
-        q1.push(root->left);
-        q2.push(root->right);
+        q.push(root->left);
+        q.push(root->right);
         
-        while(!q1.empty() && !q2.empty()){
+        while(!q.empty()){
             
-            TreeNode* n1 = q1.front();
-            TreeNode* n2 = q2.front();
+            TreeNode* left = q.front(); q.pop();
+            TreeNode* right = q.front(); q.pop();
             
-            q1.pop();
-            q2.pop();
-            
-            // Both NULL
-            if(n1 == NULL && n2 == NULL)
+            if(left == NULL && right == NULL)
                 continue;
-            
-            // One NULL
-            if(n1 == NULL || n2 == NULL)
+                
+            if(left == NULL || right == NULL)
+                return false;
+                
+            if(left->val != right->val)
                 return false;
             
-            // Values mismatch
-            if(n1->val != n2->val)
-                return false;
+            q.push(left->left);
+            q.push(right->right);
             
-            // Mirror insertion
-            q1.push(n1->left);
-            q2.push(n2->right);
-            
-            q1.push(n1->right);
-            q2.push(n2->left);
+            q.push(left->right);
+            q.push(right->left);
         }
         
         return true;
