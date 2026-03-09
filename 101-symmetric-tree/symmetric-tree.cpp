19313db14483751a5b26a5b4e65de05e8a027746
Jeevan M Swamy <jeevanms003@gmail.com>
2026-03-09 13:04:32 +0530

    Time: 0 ms (100.00%) | Memory: 18.4 MB (59.54%) - LeetSync

--- 101-symmetric-tree/symmetric-tree.cpp
+++ 101-symmetric-tree/symmetric-tree.cpp
@@ -1,36 +1,24 @@
 class Solution {
 public:
+    
+    bool isMirror(TreeNode* left, TreeNode* right){
+        
+        if(left == NULL && right == NULL)
+            return true;
+        
+        if(left == NULL || right == NULL)
+            return false;
+        
+        return (left->val == right->val) &&
+               isMirror(left->left, right->right) &&
+               isMirror(left->right, right->left);
+    }
+    
     bool isSymmetric(TreeNode* root) {
         
         if(root == NULL)
             return true;
         
-        queue<TreeNode*> q;
-        
-        q.push(root->left);
-        q.push(root->right);
-        
-        while(!q.empty()){
-            
-            TreeNode* left = q.front(); q.pop();
-            TreeNode* right = q.front(); q.pop();
-            
-            if(left == NULL && right == NULL)
-                continue;
-                
-            if(left == NULL || right == NULL)
-                return false;
-                
-            if(left->val != right->val)
-                return false;
-            
-            q.push(left->left);
-            q.push(right->right);
-            
-            q.push(left->right);
-            q.push(right->left);
-        }
-        
-        return true;
+        return isMirror(root->left, root->right);
     }
 };
