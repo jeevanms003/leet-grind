a4bcb95a59827e290034e3ad8e0e138dcf35ca30
Jeevan M Swamy <jeevanms003@gmail.com>
2026-03-10 20:24:19 +0530

    Time: 8 ms (96.08%) | Memory: 26.1 MB (47.77%) - LeetSync

---
+++ 49-group-anagrams/group-anagrams.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        
+        unordered_map<string, vector<string>> mp;
+
+        for(string s : strs) {
+            string key = s;
+            sort(key.begin(), key.end());   // create sorted key
+            mp[key].push_back(s);           // store original string
+        }
+
+        vector<vector<string>> result;
+
+        for(auto it : mp) {
+            result.push_back(it.second);
+        }
+
+        return result;
+    }
+};
