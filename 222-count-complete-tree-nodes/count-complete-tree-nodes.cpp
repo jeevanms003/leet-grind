385c0fa9554e83097bd017c33f23f957f13dcd6d
Jeevan M Swamy <jeevanms003@gmail.com>
2026-03-12 04:07:49 +0530

    Time: 0 ms (100.00%) | Memory: 31.3 MB (73.51%) - LeetSync

--- 222-count-complete-tree-nodes/count-complete-tree-nodes.cpp
+++ 222-count-complete-tree-nodes/count-complete-tree-nodes.cpp
@@ -2,9 +2,10 @@ class Solution {
 public:
     int countNodes(TreeNode* root) {
         
-        if(root == NULL)
+        if (root == NULL)
+        {
             return 0;
-        
+        }
         return 1 + countNodes(root->left) + countNodes(root->right);
     }
 };
