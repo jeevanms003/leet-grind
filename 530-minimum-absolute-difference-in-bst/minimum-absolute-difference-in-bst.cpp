964b3f319ee2f5386d4e7f27b6b591a05f16944d
Jeevan M Swamy <jeevanms003@gmail.com>
2026-03-14 01:45:47 +0530

    Time: 0 ms (100.00%) | Memory: 25.7 MB (34.14%) - LeetSync

--- 530-minimum-absolute-difference-in-bst/minimum-absolute-difference-in-bst.cpp
+++ 530-minimum-absolute-difference-in-bst/minimum-absolute-difference-in-bst.cpp
@@ -1,25 +1,32 @@
 class Solution {
 public:
-    
-    int prev = -1;
-    int ans = INT_MAX;
 
-    void inorder(TreeNode* root)
+    void dfs(TreeNode* root, vector<int>& arr)
     {
-        if(root == NULL) return;
+        if(root == NULL)
+            return;
 
-        inorder(root->left);
+        arr.push_back(root->val);
 
-        if(prev != -1)
-            ans = min(ans, root->val - prev);
-
-        prev = root->val;
-
-        inorder(root->right);
+        dfs(root->left, arr);
+        dfs(root->right, arr);
     }
 
     int getMinimumDifference(TreeNode* root) {
-        inorder(root);
+
+        vector<int> arr;
+
+        dfs(root, arr);
+
+        sort(arr.begin(), arr.end());
+
+        int ans = INT_MAX;
+
+        for(int i = 1; i < arr.size(); i++)
+        {
+            ans = min(ans, arr[i] - arr[i-1]);
+        }
+
         return ans;
     }
 };
