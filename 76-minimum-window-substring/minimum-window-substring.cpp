39be45db187bb1116e092e2716c41b041187b992
Jeevan M Swamy <jeevanms003@gmail.com>
2026-04-11 17:49:38 +0530

    Time: 8 ms (54.93%) | Memory: 11.5 MB (41.31%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.cpp
+++ 76-minimum-window-substring/minimum-window-substring.cpp
@@ -1,28 +1,32 @@
 class Solution {
 public:
     string minWindow(string s, string t) {
-        vector<int> freq(128, 0);
-        for (char c : t) freq[c]++;
+        unordered_map<char, int> mp;
+
+        // store frequency of t
+        for(char c : t) mp[c]++;
 
         int left = 0, right = 0;
-        int required = t.size();
+        int count = t.size();   // total chars needed
         int minLen = INT_MAX, start = 0;
 
-        while (right < s.size()) {
-            if (freq[s[right]] > 0)
-                required--;
-            freq[s[right]]--;
+        while(right < s.size()) {
+            // if current char is needed
+            if(mp[s[right]] > 0) count--;
+
+            mp[s[right]]--;  // include char
             right++;
 
-            while (required == 0) {
-                if (right - left < minLen) {
+            // when all characters matched
+            while(count == 0) {
+                if(right - left < minLen) {
                     minLen = right - left;
                     start = left;
                 }
 
-                freq[s[left]]++;
-                if (freq[s[left]] > 0)
-                    required++;
+                mp[s[left]]++;  // remove char
+
+                if(mp[s[left]] > 0) count++;  // needed again
                 left++;
             }
         }
