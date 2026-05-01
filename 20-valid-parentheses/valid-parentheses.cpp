f10d35ceb3f759c598a609702405b998b04378db
Jeevan M Swamy <jeevanms003@gmail.com>
2026-05-01 10:04:42 +0530

    Time: 0 ms (100.00%) | Memory: 8.8 MB (66.71%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.cpp
+++ 20-valid-parentheses/valid-parentheses.cpp
@@ -2,29 +2,20 @@ class Solution {
 public:
     bool isValid(string s) {
         stack<char> st;
-
-        for(char c : s)
-        {
-            if(c == '(' || c == '{' || c == '[')
-            {
+        for (char c : s) {
+            if (c == '(' || c == '{' || c == '[') {
                 st.push(c);
-            }
-            else
-            {
-                if(st.empty()) return false;
-
+            } else {
+                if (st.empty()) return false;
                 char top = st.top();
                 st.pop();
-
-                if((c == ')' && top != '(') ||
-                   (c == '}' && top != '{') ||
-                   (c == ']' && top != '['))
-                {
+                if ((c == ')' && top != '(') ||
+                    (c == '}' && top != '{') ||
+                    (c == ']' && top != '[')) {
                     return false;
                 }
             }
         }
-
         return st.empty();
     }
 };
