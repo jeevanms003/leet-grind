9b7f25499e40e025f95a0f58b5d3855df1f36195
Jeevan M Swamy <jeevanms003@gmail.com>
2026-06-01 18:53:31 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (53.46%) - LeetSync

---
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int climbStairs(int n) {
+        if(n<=2)
+        {
+            return n;
+        }
+
+        int p1 = 2;
+        int p2 = 1;
+
+        for(int i = 3; i<=n;i++)
+        {
+            int curr = p1 + p2;
+            p2 = p1;
+            p1 = curr;
+        }
+        return p1;
+        
+    }
+};
